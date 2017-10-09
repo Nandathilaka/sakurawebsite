1df381dc3c58b1401ca3caf5756745589fb92a8d
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2017-10-09 16:19:39 +0530

    added footer

---
+++ application/views/admin/includes/footer.php
@@ -0,0 +1,16 @@
+<!-- jQuery -->
+        <script src="<?php echo base_url()?>template/js/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="<?php echo base_url()?>template/js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="<?php echo base_url()?>template/js/metisMenu.min.js"></script>
+
+        
+
+        <!-- Custom Theme JavaScript -->
+        <script src="<?php echo base_url()?>template/js/startmin.js"></script>
+
+    </body>
+</html>
