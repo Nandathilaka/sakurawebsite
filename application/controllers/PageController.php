f484347384ac777fd234cf703ddcdca803db7429
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2017-12-16 12:55:10 +0530

    added viewLstProduct function

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class PageController extends CI_Controller{
 
+	/**
+     * This is default constructor of the class
+     */
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('productModel');
+		
+	}
 	
 	function displayHome(){
 		$this->load->view('admin/homeView');
@@ -10,6 +18,20 @@ class PageController extends CI_Controller{
 	
 	function addNewProduct(){
 		$this->load->view('admin/addProductView');
+	}
+
+	function viewLastProduct(){
+
+		$res = $this->productModel->getProduct();
+
+		$new  = array(
+						'product_title'=>$res[0]->product_title,
+						'product_price' =>$res[0]->product_price,
+						'product_image' =>$res[0]->product_image,
+						'product_discount' =>$res[0]->product_discount						
+				);
+
+		$this->load->view('admin/singleProductView',$new);
 	}
 
 	function viewProduct(){
